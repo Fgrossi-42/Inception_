685b1d1b20bb07f4c2f4bfb167192d5054710b49
Flavio <dafla107@gmail.com>
2023-08-03 19:24:07 +0200

    pollo

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -7,12 +7,12 @@ echo "[Inception-Nginx] Creating Grafana users"
 htpasswd -c -b /etc/nginx/.htpasswd-grafana $GRAFANA_USERNAME $GRAFANA_PASSWORD
 
 echo "[Inception-Nginx] Waiting Grafana..."
-while ! nc -z grafana 3000; do
+while ! nc -z grafana 3000; do   
     sleep 0.1
 done
 
 echo "[Inception-Nginx] Waiting Static Website..."
-while ! nc -z static_website 3000; do
+while ! nc -z static_website 3000; do   
     sleep 0.1
 done
 
